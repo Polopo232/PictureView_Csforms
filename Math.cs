b2990610157055991b584f5bcc60f12e99ffa08b
Ja <opilane@HP-CZC2349HTR>
2025-10-24 09:11:00 +0300

    All design in code

--- Math.cs
+++ Math.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace PictureView
@@ -9,44 +10,201 @@ namespace PictureView
 
         int addend1;
         int addend2;
-
         int minuend;
         int subtrahend;
-
         int multiplicand;
         int multiplier;
-
         int dividend;
         int divisor;
+        int timeLeft;
+
+        Label timeLabel, label1;
+        Label plusLeftLabel, plusRightLabel, label2, label3;
+        Label minusLeftLabel, minusRightLabel, label5, minus;
+        Label timesLeftLabel, timesRightLabel, label9, times;
+        Label dividedLeftLabel, dividedRightLabel, label13, quotient;
+        NumericUpDown sum, difference, product, quotientBox;
+        Button startButton;
+        Timer timer;
+
+        public Math()
+        {
+            this.Text = "Math Quiz";
+            this.ClientSize = new Size(490, 400);
+            this.FormBorderStyle = FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+
+            InitializeComponents();
+        }
+
+        private void InitializeComponents()
+        {
+            // Таймер
+            label1 = new Label()
+            {
+                Text = "Time Left",
+                Font = new Font("Microsoft Sans Serif", 15.75F),
+                Location = new Point(165, 14),
+                AutoSize = true
+            };
+            timeLabel = new Label()
+            {
+                BorderStyle = BorderStyle.FixedSingle,
+                Location = new Point(272, 9),
+                Size = new Size(200, 30),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+            Controls.Add(label1);
+            Controls.Add(timeLabel);
+
+            // +
+            plusLeftLabel = MakeLabel("?", 50, 70);
+            plusRightLabel = MakeLabel("?", 182, 70);
+            label2 = MakeLabel("+", 116, 70);
+            label3 = MakeLabel("=", 248, 70);
+            sum = MakeNumeric(314, 79);
+
+            // -
+            minusLeftLabel = MakeLabel("?", 50, 120);
+            minusRightLabel = MakeLabel("?", 182, 120);
+            minus = MakeLabel("-", 116, 120);
+            label5 = MakeLabel("=", 248, 120);
+            difference = MakeNumeric(314, 129);
+
+            // *
+            timesLeftLabel = MakeLabel("?", 50, 170);
+            timesRightLabel = MakeLabel("?", 182, 170);
+            times = MakeLabel("*", 116, 170);
+            label9 = MakeLabel("=", 248, 170);
+            product = MakeNumeric(314, 179);
+
+            // /
+            dividedLeftLabel = MakeLabel("?", 50, 220);
+            dividedRightLabel = MakeLabel("?", 182, 220);
+            quotient = MakeLabel("÷", 116, 220);
+            label13 = MakeLabel("=", 248, 220);
+            quotientBox = MakeNumeric(314, 229);
+
+            // start
+            startButton = new Button()
+            {
+                Text = "Start the quiz",
+                Font = new Font("Microsoft Sans Serif", 14F),
+                Location = new Point(187, 304),
+                AutoSize = true
+            };
+            startButton.Click += startButton_Click;
+
+            Controls.AddRange(new Control[]
+            {
+                plusLeftLabel, plusRightLabel, label2, label3, sum,
+                minusLeftLabel, minusRightLabel, minus, label5, difference,
+                timesLeftLabel, timesRightLabel, times, label9, product,
+                dividedLeftLabel, dividedRightLabel, quotient, label13, quotientBox,
+                startButton
+            });
+
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += TimerEvent;
+        }
+        private Label MakeLabel(string text, int x, int y)
+        {
+            return new Label()
+            {
+                Text = text,
+                Font = new Font("Microsoft Sans Serif", 18F),
+                Location = new Point(x, y),
+                Size = new Size(60, 50),
+                TextAlign = ContentAlignment.MiddleCenter
+            };
+        }
+
+        private NumericUpDown MakeNumeric(int x, int y)
+        {
+            return new NumericUpDown()
+            {
+                Font = new Font("Microsoft Sans Serif", 18F),
+                Location = new Point(x, y),
+                Size = new Size(120, 35)
+            };
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+        }
 
         public void StartTheQuiz()
         {
             addend1 = randomizer.Next(51);
             addend2 = randomizer.Next(51);
-
             plusLeftLabel.Text = addend1.ToString();
             plusRightLabel.Text = addend2.ToString();
-
-            sum.Text = "0";
+            sum.Value = 0;
 
             minuend = randomizer.Next(1, 101);
             subtrahend = randomizer.Next(1, minuend);
             minusLeftLabel.Text = minuend.ToString();
             minusRightLabel.Text = subtrahend.ToString();
-            difference.Text = "0";
+            difference.Value = 0;
 
             multiplicand = randomizer.Next(2, 11);
             multiplier = randomizer.Next(2, 11);
             timesLeftLabel.Text = multiplicand.ToString();
             timesRightLabel.Text = multiplier.ToString();
-            product.Text = "0";
+            product.Value = 0;
 
             divisor = randomizer.Next(2, 11);
             int temporaryQuotient = randomizer.Next(2, 11);
             dividend = divisor * temporaryQuotient;
             dividedLeftLabel.Text = dividend.ToString();
             dividedRightLabel.Text = divisor.ToString();
-            quotient.Text = "0";
+            quotientBox.Value = 0;
+
+            timeLeft = 30;
+            timeLabel.Text = "30 sekundid";
+            timer.Start();
         }
-    }   
+
+        private void TimerEvent(object sender, EventArgs e)
+        {
+            if (CheckAnswers())
+            {
+                timer.Stop();
+                MessageBox.Show("Sa vastasid kõikidele küsimustele õigesti!", "Õnnitlused!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft--;
+                timeLabel.Text = timeLeft + " sekundid";
+            }
+            else
+            {
+                timer.Stop();
+                timeLabel.Text = "Aeg on otsas!";
+                MessageBox.Show("Sa ei jõudnud õigeks ajaks valmis.", "Vabandust!");
+                ShowAnswers();
+                startButton.Enabled = true;
+            }
+        }
+
+        private bool CheckAnswers()
+        {
+            return (addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (multiplicand * multiplier == product.Value)
+                && (dividend / divisor == quotientBox.Value);
+        }
+
+        private void ShowAnswers()
+        {
+            sum.Value = addend1 + addend2;
+            difference.Value = minuend - subtrahend;
+            product.Value = multiplicand * multiplier;
+            quotientBox.Value = dividend / divisor;
+        }
+    }
 }
