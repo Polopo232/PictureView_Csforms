45035c6f02e1aede303f9d5bfd40a65cca38ea5f
opilane <opilane@HP-CZC2349HT2>
2025-10-29 17:55:08 +0200

    idk what i do last, however i made levels of difficults and fixed unicod logic

--- Match.cs
+++ Match.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace PictureView
 {
     public partial class Match : Form
     {
-
         private TableLayoutPanel tableLayoutPanel1;
         private Label[] labels;
         private Timer timer1;
+
+        private int GridSize; // <-- новое имя
 
         public Match()
         {
@@ -18,32 +20,43 @@ namespace PictureView
             this.ClientSize = new Size(534, 511);
             this.StartPosition = FormStartPosition.CenterScreen;
 
+            DialogResult difficultyChoice = MessageBox.Show(
+                "Valige raskusaste:\nYes = Lihtne\nNo = Keskmine\nCancel = Raske",
+                "Raskusastme valik",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question
+            );
+
+            if (difficultyChoice == DialogResult.Yes) GridSize = 4;
+            else if (difficultyChoice == DialogResult.No) GridSize = 6;
+            else GridSize = 10;
+
             tableLayoutPanel1 = new TableLayoutPanel
             {
                 Dock = DockStyle.Fill,
                 BackColor = SystemColors.Highlight,
                 CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset,
-                ColumnCount = 6,
-                RowCount = 6
+                ColumnCount = GridSize,
+                RowCount = GridSize
             };
 
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < GridSize; i++)
             {
-                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 16.6F));
-                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 16.6F));
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / GridSize));
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / GridSize));
             }
 
-            labels = new Label[36];
+            labels = new Label[GridSize * GridSize];
             int index = 0;
 
-            for (int row = 0; row < 6; row++)
+            for (int row = 0; row < GridSize; row++)
             {
-                for (int col = 0; col < 6; col++)
+                for (int col = 0; col < GridSize; col++)
                 {
                     Label lbl = new Label
                     {
                         Dock = DockStyle.Fill,
-                        Font = new Font("Microsoft Sans Serif", 48F),
+                        Font = new Font("Microsoft Sans Serif", 48F - (GridSize * 2)),
                         Text = "C",
                         TextAlign = ContentAlignment.MiddleCenter,
                         Name = "label" + (index + 1),
@@ -94,15 +107,39 @@ namespace PictureView
 
         private void AssignIconsToSquares()
         {
+            int totalIconsNeeded = GridSize * GridSize;
+
+            List<string> uniqueIcons = new List<string>();
+            List<string> iconsCopy = new List<string>(icons);
+
+            while (uniqueIcons.Count < totalIconsNeeded / 2 && iconsCopy.Count > 0)
+            {
+                int index = random.Next(iconsCopy.Count);
+                string icon = iconsCopy[index];
+                iconsCopy.RemoveAt(index);
+
+                if (!uniqueIcons.Contains(icon))
+                    uniqueIcons.Add(icon);
+            }
+
+            List<string> gameIcons = new List<string>();
+            foreach (string icon in uniqueIcons)
+            {
+                gameIcons.Add(icon);
+                gameIcons.Add(icon);
+            }
+
+            Shuffle(gameIcons);
+
+            int i = 0;
             foreach (Control control in tableLayoutPanel1.Controls)
             {
                 Label iconLabel = control as Label;
                 if (iconLabel != null)
                 {
-                    int randomNumber = random.Next(icons.Count);
-                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.Text = gameIcons[i];
                     iconLabel.ForeColor = iconLabel.BackColor;
-                    icons.RemoveAt(randomNumber);
+                    i++;
                 }
             }
         }
@@ -168,6 +205,14 @@ namespace PictureView
 
             MessageBox.Show("You matched all the icons!", "Congratulations");
             Close();
+        }
+        private void Shuffle<T>(List<T> list)
+        {
+            for (int i = list.Count - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                (list[i], list[j]) = (list[j], list[i]);
+            }
         }
     }
 }
