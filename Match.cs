5beb43ce4487894d90c66ce754845af75e6ea054
opilane <opilane@HP-CZC2349HT2>
2025-10-29 18:21:22 +0200

    made restart button. Idk why but it works

--- Match.cs
+++ Match.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Security.Policy;
 using System.Windows.Forms;
 
 namespace PictureView
@@ -13,6 +12,7 @@ namespace PictureView
         private Timer timer1;
 
         private int GridSize;
+        private int movesCount = 0;
 
         public Match()
         {
@@ -71,6 +71,7 @@ namespace PictureView
             }
 
             timer1 = new Timer();
+            timer1.Interval = 750;
             timer1.Tick += timer1_Tick;
 
             Controls.Add(tableLayoutPanel1);
@@ -79,9 +80,7 @@ namespace PictureView
         }
 
         Label firstClicked = null;
-
         Label secondClicked = null;
-
 
         Random random = new Random();
         List<string> icons = new List<string>()
@@ -107,23 +106,19 @@ namespace PictureView
 
         private void AssignIconsToSquares()
         {
+            List<string> iconsCopy = new List<string>(icons);
             int totalIconsNeeded = GridSize * GridSize;
 
-            List<string> uniqueIcons = new List<string>();
-            List<string> iconsCopy = new List<string>(icons);
-
-            while (uniqueIcons.Count < totalIconsNeeded / 2 && iconsCopy.Count > 0)
+            List<string> chosen = new List<string>();
+            while (chosen.Count < totalIconsNeeded / 2 && iconsCopy.Count > 0)
             {
-                int index = random.Next(iconsCopy.Count);
-                string icon = iconsCopy[index];
-                iconsCopy.RemoveAt(index);
-
-                if (!uniqueIcons.Contains(icon))
-                    uniqueIcons.Add(icon);
+                string icon = iconsCopy[random.Next(iconsCopy.Count)];
+                iconsCopy.Remove(icon);
+                chosen.Add(icon);
             }
 
             List<string> gameIcons = new List<string>();
-            foreach (string icon in uniqueIcons)
+            foreach (string icon in chosen)
             {
                 gameIcons.Add(icon);
                 gameIcons.Add(icon);
@@ -146,42 +141,43 @@ namespace PictureView
 
         private void label1_Click(object sender, EventArgs e)
         {
-            if (timer1.Enabled == true)
-                return;
+            if (timer1.Enabled) return;
 
             Label clickedLabel = sender as Label;
+            if (clickedLabel == null) return;
 
-            if (clickedLabel != null)
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
+
+            if (firstClicked == null)
             {
-                if (clickedLabel.ForeColor == Color.Black)
-                    return;
-
-                if (firstClicked == null)
-                {
-                    firstClicked = clickedLabel;
-                    firstClicked.ForeColor = Color.Black;
-                    return;
-                }
-
-                secondClicked = clickedLabel;
-                secondClicked.ForeColor = Color.Black;
-
-                CheckForWinner();
-
-                if (firstClicked.Text == secondClicked.Text)
-                {
-                    firstClicked = null;
-                    secondClicked = null;
-                    return;
-                }
-
-                timer1.Start();
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
             }
+
+            movesCount++;
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if (firstClicked != null && secondClicked != null && firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+                return;
+            }
+
+            timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             timer1.Stop();
+
+            if (firstClicked == null) return;
 
             firstClicked.ForeColor = firstClicked.BackColor;
             secondClicked.ForeColor = secondClicked.BackColor;
@@ -203,9 +199,59 @@ namespace PictureView
                 }
             }
 
-            MessageBox.Show("You matched all the icons!", "Congratulations");
-            Close();
+            DialogResult result = MessageBox.Show(
+                "You matched all the icons!\n" +
+                $"You made {movesCount} turns.\n\nDo you want to play again?",
+                "Congratulations",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information
+            );
+
+            if (result == DialogResult.Yes)
+            {
+                RestartGame();
+            }
+            else
+            {
+                Close();
+            }
         }
+
+        private void RestartGame()
+        {
+            tableLayoutPanel1.Controls.Clear();
+            movesCount = 0;
+            firstClicked = null;
+            secondClicked = null;
+
+            labels = new Label[GridSize * GridSize];
+            int index = 0;
+
+            for (int row = 0; row < GridSize; row++)
+            {
+                for (int col = 0; col < GridSize; col++)
+                {
+                    Label lbl = new Label
+                    {
+                        Dock = DockStyle.Fill,
+                        Font = new Font("Microsoft Sans Serif", 48F - (GridSize * 2)),
+                        Text = "C",
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Name = "label" + (index + 1),
+                    };
+
+                    lbl.Click += label1_Click;
+
+                    labels[index] = lbl;
+                    tableLayoutPanel1.Controls.Add(lbl, col, row);
+                    index++;
+                }
+            }
+
+            AssignIconsToSquares();
+        }
+
+
         private void Shuffle<T>(List<T> list)
         {
             for (int i = list.Count - 1; i > 0; i--)
